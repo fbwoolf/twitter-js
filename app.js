b62830211786ceee2e9a6387819dd2156b479a12
Fara Woolf <fbwoolf@gmail.com>
2017-06-12 17:01:15 -0500

    Work on post function

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-const express = require( 'express' );
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express(); // creates an instance of an express application
 
-app.get('/', function(request, response) {
-    response.send('server listening');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.use(function (req, res, next) {
+    console.log(`Verb: ${req.method} URI: ${req.url}`);
+    next( );
+});
+
+app.get('/', function (req, res) {
+    res.send('server listening');
+});
+
+app.post('/modernism', function (req, res) {
+	const newPost = req.body;
+	newPost.id = databaseOfPosts.length +1;
+	databaseOfPosts.push(newPost);
+	res.status(201).send("You created a new post" + JSON.stringify(newPost));
 });
 
 app.listen(3000, function() {
