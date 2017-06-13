1c6548ee8db1288f26e28bce62c8a4761db92c79
Fara Woolf <fbwoolf@gmail.com>
2017-06-13 09:14:18 -0500

    Post function works

--- app.js
+++ app.js
@@ -2,25 +2,39 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express(); // creates an instance of an express application
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+// const databaseOfPosts = [{id: 1, name: 'Post'}]
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
 
 app.use(function (req, res, next) {
+	//console.log('Request body: ', req.body)
+
     console.log(`Verb: ${req.method} URI: ${req.url}`);
-    next( );
+    next();
 });
 
 app.get('/', function (req, res) {
-    res.send('server listening');
+    res.send('Server is listening');
 });
 
-app.post('/modernism', function (req, res) {
-	const newPost = req.body;
-	newPost.id = databaseOfPosts.length +1;
-	databaseOfPosts.push(newPost);
-	res.status(201).send("You created a new post" + JSON.stringify(newPost));
+app.get('/posts', function (req, res) {
+    res.send('This is a post form');
 });
+
+app.post('/posts', function (req, res) {
+	console.log('Processing');
+	res.send('Processing a post');
+});
+
+// app.post('/posts', function (req, res, next) {
+// 	const newPost = req.body;
+// 	newPost.id = databaseOfPosts.length +1;
+// 	databaseOfPosts.push(newPost);
+// 	res.status(201).send('You created a new post' + JSON.stringify(newPost));
+// });
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 });
+
