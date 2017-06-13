4f9d7ee66116275de78f16167b994053207b3e1c
Min Hwangbo <min.hwangbo@gmail.com>
2017-06-13 11:18:37 -0400

    complete part 6

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 const app = express(); // creates an instance of an express application
+const nunjucks = require('nunjucks');
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione' }
+    ]
+};
+
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});
+
+
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
@@ -11,7 +34,12 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', function (req, res) {
-    res.send('server listening');
+        // res.render( 'index', {title: 'Hall of Fame', people: people} );
+    res.render('index', function(err, html) {
+        if (err) throw err;
+        res.send(html);
+    })
+    // res.send('server listening');
 });
 
 app.post('/modernism', function (req, res) {
